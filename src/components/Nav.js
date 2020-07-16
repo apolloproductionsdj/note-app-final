df8dd7ca85e079aba1781d6759785005d09ddad1
apolloproductionsdj <apolloproductionsdj@gmail.com>
2020-07-16 09:56:12 -0700

    added axios library, passed it along to List component through props of App.js and created the componenetWillMount to call the method when the page loads

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,7 @@ class Nav extends React.Component {
   render() {
     const { toggleNote, showNote } = this.props;
     return(
-      <div class="nav-container">
+      <div className="nav-container">
         <div className="nav-logo">Note</div>
         <div className="nav-button" onClick={() => toggleNote()} >
           { showNote ? 'Cancel' : '+ Note' }
