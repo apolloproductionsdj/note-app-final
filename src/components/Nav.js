37da9db8ff203934c40b6eac589d851065fc5f67
apolloproductionsdj <apolloproductionsdj@gmail.com>
2020-07-16 00:30:19 -0700

    added Nav component to App.js file

---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class Nav extends React.Component {
+
+  render() {
+    return(
+      <div class="nav-component">
+        Nav Component
+      </div>
+    )
+  }
+}
+
+
+
+
+export default Nav;
